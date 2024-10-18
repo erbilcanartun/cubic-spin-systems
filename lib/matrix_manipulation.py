9bc8c741f998dc427eac546d6eb7d5b9d5a6e9df
Erbil Can Artun <49557189+erbilcanartun@users.noreply.github.com>
2024-10-18 09:51:44 +0300

    Add matrix_average method

--- lib/matrix_manipulation.py
+++ lib/matrix_manipulation.py
@@ -5,16 +5,16 @@ def normalizer(x):
     return x / np.amax(x)
 
 def main_diagonal(x):
-    
+
     n = len(x)
-    
+
     if type(x) == (np.ndarray or list):
         diag = np.array([])
         for i in range(n):
             for j in range(n):
                 if i == j:
                     diag = np.append(diag, x[i][j])
-                    
+
     if type(x) == mp.matrix:
         diag = np.array([])
         for i in range(n):
@@ -24,14 +24,14 @@ def main_diagonal(x):
     return diag
 
 def anti_diagonal(x):
-    
+
     n = len(x)
-    
+
     if type(x) == (np.ndarray or list):
         antidiag = np.array([])
         for i in range(n):
                 antidiag = np.append(antidiag, x[i][n - 1 - i])
-                
+
     if type(x) == mp.matrix:
         antidiag = np.array([])
         for i in range(n):
@@ -43,9 +43,23 @@ def off_diagonal(x):
     return np.extract(1 - np.eye(len(x)), x)
 
 def off_anti_diagonal(x):
-    
+
     n = len(x)
     a = np.ones((n, n))
     np.fill_diagonal(np.fliplr(a), 0)
-    
+
     return np.extract(a, x)
+
+def matrix_average(matrices):
+
+    n = len(matrices[0])
+    N = len(matrices)
+    t0 = mp.matrix(n)
+
+    for i in range(n):
+        for j in range(n):
+            c = 0
+            for t in matrices:
+                c += t[i, j]
+            t0[i, j] = c / N
+    return t0
