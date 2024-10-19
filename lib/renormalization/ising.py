b9237d1ea44c613274724c8f4ec2f7c52fe60c26
Erbil Can Artun <49557189+erbilcanartun@users.noreply.github.com>
2024-10-19 13:31:39 +0300

    Add phase method that determines phase by renormalization-group sink

--- lib/renormalization/ising.py
+++ lib/renormalization/ising.py
@@ -26,45 +26,45 @@ class CubicIsingModel:
             if n == 1:
                 if u == 'x':
                     t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j)],
-                                   [mp.exp(-j), mp.exp(j - 2*h)]])
+                                   [mp.exp(-j),      mp.exp(j - 2*h)]])
 
             elif n == 2:
                 if u == 'x':
-                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j), mp.mpf(1), mp.mpf(1)],
-                                   [mp.exp(-j), mp.exp(j - 2*h), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(j), mp.exp(-j)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(-j), mp.exp(j)]])
+                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j),      mp.exp(h),  mp.exp(h)],
+                                   [mp.exp(-j),      mp.exp(j - 2*h), mp.exp(-h),  mp.exp(-h)],
+                                   [mp.exp(h),       mp.exp(-h),       mp.exp(j),  mp.exp(-j)],
+                                   [mp.exp(h),       mp.exp(-h),       mp.exp(-j), mp.exp(j)]])
 
                 if u == 'xy':
-                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j), mp.mpf(1), mp.mpf(1)],
-                                   [mp.exp(-j), mp.exp(j - 2*h), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(j + 2*h), mp.exp(-j)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(-j), mp.exp(j - 2*h)]])
+                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j),      mp.mpf(2 * h),   mp.mpf(1)],
+                                   [mp.exp(-j),      mp.exp(j - 2*h), mp.mpf(1),       mp.exp(-2 * h)],
+                                   [mp.exp(2 * h),   mp.mpf(1),       mp.exp(j + 2*h), mp.exp(-j)],
+                                   [mp.mpf(1),       mp.exp(-2 * h),  mp.exp(-j),      mp.exp(j - 2*h)]])
 
             elif n == 3:
                 if u == 'x':
-                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1)],
-                                   [mp.exp(-j), mp.exp(j - 2*h), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(j), mp.exp(-j), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(-j), mp.exp(j), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.exp(j), mp.exp(-j)],
-                                   [mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.exp(-j), mp.exp(j)]])
+                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j),      mp.exp(h),  mp.exp(h),  mp.exp(h),  mp.exp(h)],
+                                   [mp.exp(-j),      mp.exp(j - 2*h), mp.exp(-h), mp.exp(-h), mp.exp(-h), mp.exp(-h)],
+                                   [mp.exp(h),       mp.exp(-h),      mp.exp(j),  mp.exp(-j), mp.mpf(1),  mp.mpf(1)],
+                                   [mp.exp(h),       mp.exp(-h),      mp.exp(-j), mp.exp(j),  mp.mpf(1),  mp.mpf(1)],
+                                   [mp.exp(h),       mp.exp(-h),      mp.mpf(1),  mp.mpf(1),  mp.exp(j),  mp.exp(-j)],
+                                   [mp.exp(h),       mp.exp(-h),      mp.mpf(1),  mp.mpf(1),  mp.exp(-j), mp.exp(j)]])
 
                 if u == 'xy':
-                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1)],
-                                   [mp.exp(-j), mp.exp(j - 2*h), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(j + 2*h), mp.exp(-j), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(-j), mp.exp(j - 2*h), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.exp(j), mp.exp(-j)],
-                                   [mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.exp(-j), mp.exp(j)]])
+                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j),      mp.exp(2 * h),   mp.mpf(1),       mp.exp(h),  mp.exp(h)],
+                                   [mp.exp(-j),      mp.exp(j - 2*h), mp.mpf(1),       mp.exp(-2 * h),  mp.exp(-h), mp.exp(-h)],
+                                   [mp.exp(2 * h),   mp.mpf(1),       mp.exp(j + 2*h), mp.exp(-j),      mp.exp(h),  mp.exp(h)],
+                                   [mp.mpf(1),       mp.exp(-2 * h),  mp.exp(-j),      mp.exp(j - 2*h), mp.exp(-h), mp.exp(-h)],
+                                   [mp.exp(h),       mp.exp(-h),      mp.exp(h),       mp.exp(-h),      mp.exp(j),  mp.exp(-j)],
+                                   [mp.exp(h),       mp.exp(-h),      mp.exp(h),       mp.exp(-h),      mp.exp(-j), mp.exp(j)]])
 
                 if u == 'xyz':
-                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1)],
-                                   [mp.exp(-j), mp.exp(j - 2*h), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(j + 2*h), mp.exp(-j), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.exp(-j), mp.exp(j - 2*h), mp.mpf(1), mp.mpf(1)],
-                                   [mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.exp(j + 2*h), mp.exp(-j)],
-                                   [mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.exp(-j), mp.exp(j - 2*h)]])
+                    t = mp.matrix([[mp.exp(j + 2*h), mp.exp(-j),      mp.exp(2 * h),   mp.mpf(1),        mp.exp(2 * h),   mp.mpf(1)],
+                                   [mp.exp(-j),      mp.exp(j - 2*h), mp.mpf(1),       mp.exp(-2 * h),   mp.mpf(1),       mp.exp(-2 * h)],
+                                   [mp.exp(2 * h),   mp.mpf(1),       mp.exp(j + 2*h), mp.exp(-j),       mp.exp(2 * h),   mp.mpf(1)],
+                                   [mp.mpf(1),       mp.mpf(-2 * h),  mp.exp(-j),      mp.exp(j - 2*h),  mp.mpf(1),       mp.exp(-2 * h)],
+                                   [mp.exp(2 * h),   mp.mpf(1),       mp.exp(2 * h),   mp.mpf(1),        mp.exp(j + 2*h), mp.exp(-j)],
+                                   [mp.mpf(1),       mp.mpf(-2 * h),  mp.mpf(1),       mp.exp(-2 * h),   mp.exp(-j),      mp.exp(j - 2*h)]])
 
             elif n == 4:
                 t = mp.matrix([[mp.exp(j), mp.exp(-j), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1), mp.mpf(1)],
@@ -116,9 +116,8 @@ class IsingRenormalizationGroup(CubicIsingModel):
 
         for i in range(n - 1):
             t = mp_multiply(t, matrices[i + 1])
-        t = normalizer(t)
 
-        return t
+        return normalizer(t)
 
     def _decimation(self, matrices):
         t = matrices[0] * matrices[1]
@@ -152,7 +151,7 @@ class IsingRenormalizationGroup(CubicIsingModel):
         return renormalized
 
     def _deviator_transfer_matrix(self, j):
-        # Creates a transfer matrix with zero field
+        # Creates a zero-field transfer matrix
         J = mp.mpf(j)
 
         n = 2 * self.n
@@ -216,3 +215,158 @@ class IsingRenormalizationGroup(CubicIsingModel):
                 deviated = self._bond_moving(dm)
 
         return renormalized, interaction(normal), interaction(deviated)
+
+    def phase(self):
+
+        #limsup = 0.95 * lattice_size
+        #liminf = 0.05 * lattice_size
+
+        matrices = self.transfer_matrices
+        phase = "undetermined"
+        e = 1e-3
+        RG = 20 # RG iteration number
+
+        if self.n == 1:
+            for k in range(RG):
+
+                matrices = self.renormalize(matrices)
+
+                t = matrix_average(matrices)
+                tsum = np.sum(t)
+                diagsum = np.sum(main_diagonal(t))
+                offdiagsum = np.sum(off_diagonal(t))
+
+                if 4 - e < tsum < 4 + e:
+                    phase = "disorder"
+                    break
+
+                # Main diagonal 1, others 0 (zero field)
+                elif 2 - e < t[0, 0] + t[1, 1] < 2 + e and t[0, 1] + t[1, 0] < e:
+                    phase = "ferro"
+                    break
+
+                # Anti diagonal 1, others 0 (zero field)
+                elif 2 - e < t[0, 1] + t[1, 0] < 2 + e and t[0, 0] + t[1, 1] < e:
+                    phase = "antiferro"
+                    break
+
+                # First element 1, others 0
+                elif 1 - e < t[0, 0] < 1 + e and t[0, 1] + t[1, 0] + t[1, 1] < e:
+                    phase = "x_aligned"
+                    break
+
+                # Last element 1, others 0
+                elif 1 - e < t[1, 1] < 1 + e and t[0, 1] + t[1, 0] + t[0, 0] < e:
+                    phase = "x_antialigned"
+                    break
+
+                else:
+                    phase = "unknown"
+
+        if self.n == 2:
+            for k in range(RG):
+
+                matrices = self.renormalize(matrices)
+
+                t = matrix_average(matrices)
+                tsum = np.sum(t)
+                diagsum = np.sum(main_diagonal(t))
+                offdiagsum = np.sum(off_diagonal(t))
+
+                if 16 - e < tsum < 16 + e:
+                    phase = "disorder"
+                    break
+
+                elif 4 - e < diagsum < 4 + e and offdiagsum < e:
+                    phase = "ferro"
+                    break
+
+                elif 1 - e < tsum < 1 + e and 1 - e < t[0, 0] < 1 + e:
+                    phase = "x_aligned"
+                    break
+
+                elif 4 - e < tsum < 4 + e and 4 - e < t[0, 0] + t[0, 2] + t[2, 0] + t[2, 2] < 4 + e:
+                    phase = "xy_aligned"
+                    break
+
+                elif 2 - e < tsum < 2 + e and 2 - e < t[0, 0] + t[2, 2] < 2 + e:
+                    phase = "xory_aligned"
+                    break
+
+                elif 2 - e < tsum < 2 + e and 2 - e < t[0, 2] + t[2, 0] < 2 + e:
+                    phase = "xy_alternating"
+                    break
+
+                elif 2 - e < tsum < 2 + e and 2 - e < t[0, 1] + t[1, 0] < 2 + e:
+                    phase = "x_antiferro"
+                    break
+
+                elif 4 - e < tsum < 4 + e and 4 - e < t[0, 1] + t[1, 0] + t[2, 3] + t[3, 2] < 4 + e:
+                    phase = "xory_antiferro"
+                    break
+
+                else:
+                    phase = "unknown"
+
+        if self.n == 3:
+            for k in range(RG):
+
+                matrices = self.renormalize(matrices)
+
+                t = matrix_average(matrices)
+                tsum = np.sum(t)
+                diagsum = np.sum(main_diagonal(t))
+                offdiagsum = np.sum(off_diagonal(t))
+
+                if 36 - e < tsum < 36 + e:
+                    phase = "disorder"
+                    break
+
+                elif 6-e < diagsum < 6+e and offdiagsum < e:
+                    phase = "ferro"
+                    break
+
+                elif 1-e < tsum < 1+e and 1-e < t[0, 0] < 1+e:
+                    phase = "x_aligned"
+                    break
+
+                elif 4-e < tsum < 4+e and 4-e < t[0, 0] + t[0, 2] + t[2, 0] + t[2, 2] < 4+e:
+                    phase = "xy_aligned"
+                    break
+
+                elif 9 - e < tsum < 9 + e and 9 - e < t[0, 0] + t[0, 2] + t[0, 4] + t[2, 0] + t[2, 2] + t[2, 4] + t[4, 0] + t[4, 2] + t[4, 4] < 9 + e:
+                    phase = "xyz_aligned"
+                    break
+
+                elif 2 - e < tsum < 2 + e and 2 - e < t[0, 0] + t[2, 2] < 2 + e:
+                    phase = "xory_aligned"
+                    break
+
+                elif 3 - e < tsum < 3 + e and 3 - e < t[0, 0] + t[2, 2] + t[4, 4] < 3 + e:
+                    phase = "xoryorz_aligned"
+                    break
+
+                elif 2 - e < tsum < 2 + e and 2 - e < t[0, 2] + t[2, 0] < 2 + e:
+                    phase = "xy_alternating"
+                    break
+
+                elif 2 - e < tsum < 2 + e and 2 - e < t[0, 1] + t[1, 0] < 2 + e:
+                    phase = "x_antiferro"
+                    break
+
+                elif 4 - e < tsum < 4 + e and 4 - e < t[0, 1] + t[1, 0] + t[2, 3] + t[3, 2] < 4 + e:
+                    phase = "xory_antiferro"
+                    break
+
+                elif 6 - e < tsum < 6 + e and 6 - e < t[0, 1] + t[1, 0] + t[2, 3] + t[3, 2] + t[4, 5] + t[5, 4] < 6 + e:
+                    phase = "xoryorz_antiferro"
+                    break
+
+                elif 12 - e < tsum < 12 + e and 12 - e < t[0, 0] + t[0, 1] + t[1, 0] + t[1, 1] + t[2, 2] + t[2, 3] + t[3, 2] + t[3, 3] + t[4, 4] + t[4, 5] + t[5, 4] + t[5, 5] < 12 + e:
+                    phase = "nematic"
+                    break
+
+                else:
+                    phase = "unknown"
+
+        return k, phase
